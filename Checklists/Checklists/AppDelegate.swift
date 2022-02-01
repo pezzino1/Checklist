af5ecef0be1963dfa6b2cdc536c0d391daee0805
pezzino1 <63758294+pezzino1@users.noreply.github.com>
2022-01-31 20:42:02 -0500

    Add files via upload

---
+++ Checklists/Checklists/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  Checklists
+//
+//  Created by Vinnie Pezzino on 1/19/22.
+//
+
+import UIKit
+import UserNotifications
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Local notifications
+    let center = UNUserNotificationCenter.current()
+    center.delegate = self
+    return true
+  }
+
+  // MARK: - User Notification Delegates
+  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    print("Received local notification \(notification)")
+  }
+
+  // MARK: UISceneSession Lifecycle
+
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+
+  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+  }
+}
---
+++ Checklists/Checklists/Data Models/ChecklistItem.swift
@@ -0,0 +1,48 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Vinnie Pezzino on 1/21/22.
+//
+
+import Foundation
+import UserNotifications
+
+class ChecklistItem: NSObject, Codable {
+  var text = ""
+  var checked = false
+  var dueDate = Date()
+  var shouldRemind = false
+  var itemID = -1
+
+  override init() {
+    super.init()
+    itemID = DataModel.nextChecklistItemID()
+  }
+
+  deinit {
+    removeNotification()
+  }
+
+  func scheduleNotification() {
+    removeNotification()
+    if shouldRemind && dueDate > Date() {
+      let content = UNMutableNotificationContent()
+      content.title = "Reminder:"
+      content.body = text
+      content.sound = UNNotificationSound.default
+
+      let calendar = Calendar(identifier: .gregorian)
+      let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: dueDate)
+      let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+      let request = UNNotificationRequest(identifier: "\(itemID)", content: content, trigger: trigger)
+      let center = UNUserNotificationCenter.current()
+      center.add(request)
+    }
+  }
+
+  func removeNotification() {
+    let center = UNUserNotificationCenter.current()
+    center.removePendingNotificationRequests(withIdentifiers: ["\(itemID)"])
+  }
+}
